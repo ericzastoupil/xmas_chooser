a12d40f6294b3d5edabba84a380e857a994e272b
ericzastoupil <ericzastoupil@users.noreply.github.com>
2023-11-22 21:07:27 -0500

    removing spouse from participant attributes

--- participant.py
+++ participant.py
@@ -1,6 +1,5 @@
 class Participant():
-    def __init__(self, name, spouse, email, dont_gift):
+    def __init__(self, name, email, dont_gift):
         self.name = name
-        self.spouse = spouse
         self.email = email
         self.dont_gift = dont_gift
